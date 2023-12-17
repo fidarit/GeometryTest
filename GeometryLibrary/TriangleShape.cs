27afa9ab9c5afa77ee1481c898e529400b796ab1
Fidarit Mullayanov <fidarit.mullayanov@yandex.ru>
2023-12-18 01:01:28 +0500

    added triangle

---
+++ GeometryLibrary/TriangleShape.cs
@@ -0,0 +1,60 @@
+﻿namespace GeometryLibrary
+{
+    public class TriangleShape : Shape
+    {
+        public readonly double a;
+        public readonly double b;
+        public readonly double c;
+
+        /// <summary>
+        /// Создание треугольника по трем сторонам
+        /// </summary>
+        /// <exception cref="ArgumentException">Генерируется, если переданы некорректные значения сторон</exception>
+        public TriangleShape(double a, double b, double c)
+        {
+            if (!IsValidTriangle(a, b, c))
+            {
+                throw new ArgumentException("Некорректные значения сторон треугольника.");
+            }
+
+            this.a = a; 
+            this.b = b; 
+            this.c = c;
+        }
+
+        public override double GetArea()
+        {
+            // Используем формулу Герона
+            double p = (a + b + c) / 2;
+            return Math.Sqrt(p * (p - a) * (p - b) * (p - c));
+        }
+
+        public override double GetPerimeter()
+        {
+            return a + b + c;
+        }
+
+        /// <summary>
+        /// Является ли треугольник прямоугольным
+        /// </summary>
+        public bool IsRightTriangle(double eps = double.Epsilon)
+        {
+            double[] sides = { a, b, c };
+            Array.Sort(sides);
+
+            // Используем теорему Пифагора
+            double left = Math.Pow(sides[0], 2) + Math.Pow(sides[1], 2);
+            double right = Math.Pow(sides[2], 2);
+
+            return Math.Abs(left - right) <= eps;
+        }
+
+        /// <summary>
+        /// Проверка, что переданные значения образуют действительный треугольник
+        /// </summary>
+        private static bool IsValidTriangle(double a, double b, double c)
+        {
+            return a + b > c && a + c > b && b + c > a;
+        }
+    }
+}
--- GeometryLibraryTests/TriangleUnitTests.cs
+++ GeometryLibraryTests/TriangleUnitTests.cs
@@ -1,11 +1,46 @@
+using GeometryLibrary;
+
 namespace GeometryLibraryTests
 {
     public class TriangleUnitTests
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData(1, 2, 5)]
+        [InlineData(1, 2, 3)]
+        public void Constructor_InvalidSides_ThrowsArgumentException(double a, double b, double c)
         {
+            Assert.Throws<ArgumentException>(() => new TriangleShape(a, b, c));
+        }
 
+        [Theory]
+        [InlineData(3, 4, 5, 6)]
+        public void GetArea_ReturnsCorrectResult(double a, double b, double c, double expectedArea)
+        {
+            var triangle = new TriangleShape(a, b, c);
+            double result = triangle.GetArea();
+
+            Assert.Equal(expectedArea, result, 3);
+        }
+
+        [Theory]
+        [InlineData(3, 4, 5, 12)]
+        public void GetPerimeter_ReturnsCorrectResult(double a, double b, double c, double expectedPerimeter)
+        {
+            var triangle = new TriangleShape(a, b, c);
+            double result = triangle.GetPerimeter();
+
+            Assert.Equal(expectedPerimeter, result, 3);
+        }
+
+        [Theory]
+        [InlineData(3, 4, 5, true)]
+        [InlineData(2, 3, 4, false)]
+        public void IsRightTriangle_ReturnsCorrectResult(double a, double b, double c, bool expectedResult)
+        {
+            var triangle = new TriangleShape(a, b, c);
+            bool result = triangle.IsRightTriangle();
+
+            Assert.Equal(expectedResult, result);
         }
     }
 }
