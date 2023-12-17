bb84b975fa4ef8dd9205d1077d9c06cecc78f93f
Fidarit Mullayanov <fidarit.mullayanov@yandex.ru>
2023-12-18 01:09:49 +0500

    added circle | first task is ready

---
+++ GeometryLibrary/CircleShape.cs
@@ -0,0 +1,31 @@
+﻿namespace GeometryLibrary
+{
+    public class CircleShape : Shape
+    {
+        public readonly double Radius;
+
+        /// <summary>
+        /// Создание окружности по радиусу
+        /// </summary>
+        /// <exception cref="ArgumentException">Генерируется, если передан отрицательный радиус</exception>
+        public CircleShape(double radius)
+        {
+            if (radius < 0)
+            {
+                throw new ArgumentException("Радиус не может быть отрицательным.");
+            }
+
+            Radius = radius;
+        }
+
+        public override double GetArea()
+        {
+            return Math.PI * Math.Pow(Radius, 2);
+        }
+
+        public override double GetPerimeter()
+        {
+            return 2 * Math.PI * Radius;
+        }
+    }
+}
---
+++ GeometryLibraryTests/CircleUnitTests.cs
@@ -0,0 +1,37 @@
+﻿using GeometryLibrary;
+
+namespace GeometryLibraryTests
+{
+    public class CircleUnitTests
+    {
+        [Theory]
+        [InlineData(-1)]
+        [InlineData(-10)]
+        public void Constructor_NegativeRadius_ThrowsArgumentException(double radius)
+        {
+            Assert.Throws<ArgumentException>(() => new CircleShape(radius));
+        }
+
+        [Theory]
+        [InlineData(2, 12.566)]
+        [InlineData(3, 28.274)]
+        public void GetArea_ReturnsCorrectResult(double radius, double expectedArea)
+        {
+            var circle = new CircleShape(radius);
+            double actualArea = circle.GetArea();
+
+            Assert.Equal(expectedArea, actualArea, 3);
+        }
+
+        [Theory]
+        [InlineData(2, 12.566)]
+        [InlineData(3, 18.85)]
+        public void GetPerimeter_ReturnsCorrectResult(double radius, double expectedPerimeter)
+        {
+            var circle = new CircleShape(radius);
+            double actualPerimeter = circle.GetPerimeter();
+
+            Assert.Equal(expectedPerimeter, actualPerimeter, 3);
+        }
+    }
+}
